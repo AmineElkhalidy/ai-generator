5e7c87be00079ccbf37617d7b26fdb769a023eaf
Amine Elkhalidy <a.amineelkhalidy@gmail.com>
2024-02-15 10:47:57 +0100

    3- Clerk for authentication

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 const LandingPage = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Link href="/sign-in">
+        <Button size="lg">Login</Button>
+      </Link>
+
+      <Link href="/sign-up">
+        <Button size="lg">Register</Button>
+      </Link>
+    </div>
+  );
 };
 
 export default LandingPage;
