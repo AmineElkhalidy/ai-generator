3bb710010498fd6544123f8a5edbad6d31c4db3a
Amine Elkhalidy <a.amineelkhalidy@gmail.com>
2024-02-16 18:39:42 +0100

    9- Image UI & API added!

--- app/(dashboard)/(routes)/image/page.tsx
+++ app/(dashboard)/(routes)/image/page.tsx
@@ -1,12 +1,13 @@
 "use client";
 
 import Heading from "@/components/Heading";
-import { Image } from "lucide-react";
+import Image from "next/image";
+import { ImageIcon, Download } from "lucide-react";
 import { useForm } from "react-hook-form";
 import * as z from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import React, { useState } from "react";
-import { ConversationFormSchema } from "@/schemas";
+import { ImageFormSchema } from "@/schemas";
 import { Button } from "@/components/ui/button";
 import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
@@ -14,23 +15,41 @@ import axios from "axios";
 import { useRouter } from "next/navigation";
 import Empty from "@/components/Empty";
 import Loader from "@/components/Loader";
+import { amountOptions, resolutionOptions } from "@/schemas";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Card, CardFooter } from "@/components/ui/card";
 
 const ImageGenerationPage = () => {
   const router = useRouter();
   const [images, setImages] = useState<string[]>([]);
 
-  const form = useForm<z.infer<typeof ConversationFormSchema>>({
-    resolver: zodResolver(ConversationFormSchema),
+  const form = useForm<z.infer<typeof ImageFormSchema>>({
+    resolver: zodResolver(ImageFormSchema),
     defaultValues: {
       prompt: "",
+      amount: "1",
+      resolution: "512x512",
     },
   });
 
   const isLoading = form.formState.isSubmitting;
 
-  async function onSubmit(values: z.infer<typeof ConversationFormSchema>) {
+  async function onSubmit(values: z.infer<typeof ImageFormSchema>) {
     try {
-      const response = await axios.post("/api/conversation");
+      setImages([]);
+
+      const response = await axios.post("/api/image", values);
+
+      // Extract the url of each image
+      const urls = response.data.map((image: { url: string }) => image.url);
+
+      setImages(urls);
 
       form.reset();
     } catch (error) {
@@ -45,7 +64,7 @@ const ImageGenerationPage = () => {
       <Heading
         title="Image Generation"
         description="Turn your prompt into an image."
-        Icon={Image}
+        Icon={ImageIcon}
         iconColor="text-pink-700"
         bgColor="bg-pink-700/10"
       />
@@ -58,28 +77,79 @@ const ImageGenerationPage = () => {
               className="rounded-lg border w-full p-4 px-3 md:px-6 focus-within:shadow-sm grid grid-cols-12 gap-2"
             >
               <FormField
-                control={form.control}
                 name="prompt"
                 render={({ field }) => (
-                  <FormItem className="col-span-12 lg:col-span-10">
+                  <FormItem className="col-span-12 lg:col-span-6">
                     <FormControl className="m-0 p-0">
                       <Input
                         className="border-0 outline-none focus-visible:ring-0 focus-visible:ring-transparent"
-                        placeholder="Have a conversation with Genify..."
                         disabled={isLoading}
+                        placeholder="A picture of a horse in Swiss alps"
                         {...field}
-                        autoComplete="off"
-                        type="text"
                       />
                     </FormControl>
                   </FormItem>
                 )}
               />
-
+              <FormField
+                control={form.control}
+                name="amount"
+                render={({ field }) => (
+                  <FormItem className="col-span-12 lg:col-span-2">
+                    <Select
+                      disabled={isLoading}
+                      onValueChange={field.onChange}
+                      value={field.value}
+                      defaultValue={field.value}
+                    >
+                      <FormControl>
+                        <SelectTrigger>
+                          <SelectValue defaultValue={field.value} />
+                        </SelectTrigger>
+                      </FormControl>
+                      <SelectContent>
+                        {amountOptions.map((option) => (
+                          <SelectItem key={option.value} value={option.value}>
+                            {option.label}
+                          </SelectItem>
+                        ))}
+                      </SelectContent>
+                    </Select>
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="resolution"
+                render={({ field }) => (
+                  <FormItem className="col-span-12 lg:col-span-2">
+                    <Select
+                      disabled={isLoading}
+                      onValueChange={field.onChange}
+                      value={field.value}
+                      defaultValue={field.value}
+                    >
+                      <FormControl>
+                        <SelectTrigger>
+                          <SelectValue defaultValue={field.value} />
+                        </SelectTrigger>
+                      </FormControl>
+                      <SelectContent>
+                        {resolutionOptions.map((option) => (
+                          <SelectItem key={option.value} value={option.value}>
+                            {option.label}
+                          </SelectItem>
+                        ))}
+                      </SelectContent>
+                    </Select>
+                  </FormItem>
+                )}
+              />
               <Button
+                className="col-span-12 lg:col-span-2 w-full"
                 type="submit"
                 disabled={isLoading}
-                className="col-span-12 lg:col-span-2 w-full"
+                size="icon"
               >
                 Generate
               </Button>
@@ -100,8 +170,24 @@ const ImageGenerationPage = () => {
             </div>
           )}
 
-          <div className="flex flex-col-reverse gap-y-4">
-            <div>Images will be rendered soon</div>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 mt-8">
+            {images.map((src) => (
+              <Card key={src} className="rounded-lg overflow-hidden">
+                <div className="relative aspect-square">
+                  <Image fill alt="Generated photo" src={src} />
+                </div>
+                <CardFooter className="p-2">
+                  <Button
+                    onClick={() => window.open(src)}
+                    variant="secondary"
+                    className="w-full"
+                  >
+                    <Download className="h-4 w-4 mr-2" />
+                    Download
+                  </Button>
+                </CardFooter>
+              </Card>
+            ))}
           </div>
         </div>
       </div>
