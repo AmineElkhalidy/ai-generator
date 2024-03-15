86a9b5d4f1ccce23cbd5ab204d81a9a006683ac3
Amine Elkhalidy <a.amineelkhalidy@gmail.com>
2024-03-15 19:36:45 +0000

    19- Slight modification added!

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -7,7 +7,9 @@ export const metadata: Metadata = {
 
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="h-full flex items-center justify-center">{children}</div>
+    <div className="min-h-screen flex items-center justify-center py-10">
+      {children}
+    </div>
   );
 };
 
